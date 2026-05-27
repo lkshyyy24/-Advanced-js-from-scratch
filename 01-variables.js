b420110998178099182ca7301ae2f6fc681a212e
Lakshay Arora <iamlakshay24@gmail.com>
2026-05-27 16:38:29 +0000

    learned variables

---
+++ 01-variables.js
@@ -0,0 +1,7 @@
+let accountID = 123456;
+let name = "lakshay";
+
+name = "alisha"
+accountID = "2405"
+
+console.table({accountID,name});
---
+++ 02-datatypes.js
@@ -0,0 +1,14 @@
+let age = 21;
+let name = "lakshay";
+
+console.log("name is",name)
+console.log("age is" ,age);
+
+console.log( "and age is",typeof age);
+
+let Age = String(age);
+console.log(typeof Age);
+
+console.log("2" == 2);
+console.log("2" === 2 ); // it compares strictly 
+
