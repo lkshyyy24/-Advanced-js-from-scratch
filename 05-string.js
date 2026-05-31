803524881bfa622f7facc3a7af1d39b5bf678027
Lakshay Arora <iamlakshay24@gmail.com>
2026-05-31 19:32:17 +0000

    today i learned about numbers and maths in js

---
+++ 05-string.js
@@ -0,0 +1,9 @@
+let char = "lakshay"
+
+console.log(char.charAt(3))
+// thats how you can declare the string at nth index 
+
+const url = "https://lakshay.com&arora"
+
+console.log(url.replace('&' , '-'))
+//here i learnt how to use replace 
---
+++ numsAndmaths.js
@@ -0,0 +1,27 @@
+ const number = 400;
+
+ console.log(number.toString().length) //here is how we can convert nyumbers to string 
+
+ const newNumber = 100000
+
+ console.log(newNumber.toPrecision(7)); // used to get precise value
+ 
+ console.log(newNumber.toLocaleString());//this will put comma and it will help us to read in ease 
+ 
+
+
+ // ======================================================================
+
+
+ const againNumber = 4.9
+
+ console.log(Math.round(7.8));
+
+ // if we want min value = 10 and max value 20 
+
+ const min = 10
+ const max = 20 
+
+ console.log(Math.floor(Math.random()*( max-min+1 )+min)) 
+ 
+ 
