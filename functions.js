db86a19a36a09a4293bd5e8f90ee40d967a81d40
Lakshay Arora <iamlakshay24@gmail.com>
2026-06-02 09:11:57 +0000

    learned functions

---
+++ functions.js
@@ -0,0 +1,43 @@
+function SayMyName () {
+    console.log("L");
+    console.log("a");
+    console.log("k");
+    console.log("s");
+    console.log("h");
+    console.log("a");
+    console.log("y");
+    
+}
+
+SayMyName()
+
+//adding two numbers 
+
+
+function addNumbers (num1,num2){
+console.log(num1 + num2);
+
+}
+
+addNumbers(2,4)
+
+//
+
+function userName (username) {
+return `${username} is learning js `
+}
+console.log(userName("lakshay"));
+
+
+const price = { // object
+username : "laksay",
+price : 200
+}
+
+function getName (usser){ //parameter 
+console.log(`the price is ${usser.price} and buyer name is ${usser.username}`);
+
+}
+getName(price)//call
+
+// here we have to define the object name not the function name or declaration 
